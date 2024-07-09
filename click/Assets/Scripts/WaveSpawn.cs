afd3527988c68e2ea36649eb1cba6e124bc85a1b
Daniel Mendes <80532809+danielregismendes@users.noreply.github.com>
2024-07-09 20:00:10 -0300

    0.004
    Spawn de Waves implementado:
    
    -Ponto inicial personalizável.
    -Rotas de inimigos personalizadas.
    -Quantidade, tipo de inimigo e tempo entre spawns personalizados.

--- click/Assets/Scripts/WaveSpawn.cs
+++ click/Assets/Scripts/WaveSpawn.cs
@@ -11,78 +11,109 @@ public class WaveSpawn : MonoBehaviour
     public WaveData[] waves;
 
     private GameObject currentEnemy;
-    private int spawnOrder = 0;
+    private int waveOrder = 0;
+    private int currentWave = 0;
+    private int currentSpawn = 0;
+    private int currentSpawnEnemy = 0;
+    private float timer = 0;
+    private float timerSpawn = 0;
 
+
+    private void Update()
+    {
+        SpawnWave();
+
+        Debug.Log("currentWave: " + currentWave);
+        Debug.Log("currentSpawn: " + currentSpawn);
+        Debug.Log("currentSpawnEnemy: " + currentSpawnEnemy);
+
+    }
 
     public void NextSpawn()
     {
-        SpawnWave(spawnOrder);
-        spawnOrder++;
+
+        waveOrder++;
+
     }
 
-    void SpawnWave(int waveOrder)
+    void SpawnWave()
     {
-        if(waveOrder > waves.Length)
+        if (currentWave <= waveOrder)
         {
 
-            return;
-
-        }
-        else
-        {
-            Debug.Log("Else");
-            for (int i = 0; i < waves[waveOrder].spawns.Length; i++)
+            if (currentSpawn + 1 <= waves[currentWave].spawns.Length)
             {
-                /*
-                for (float time = 0; time < waves[waveOrder].spawns[i].timeWaiting;)
+
+                if (timer > waves[currentWave].spawns[currentSpawn].timeWaiting)
                 {
 
-                    time =+ 1 * Time.deltaTime;
-                    Debug.Log("Timer 1" + time);
-
-                }*/
-
-                for (int qtd = 0; qtd < waves[waveOrder].spawns[i].timeWaiting; qtd++)
-                {
-
-                    currentEnemy = Instantiate(waves[waveOrder].spawns[i].enemy, transform.position, transform.rotation);
-                    currentEnemy.GetComponent<Enemy>().SetRota(waves[waveOrder].spawns[i].rota);
-
-                    
-                    /*
-                    for (float time = 0; time < timeSpawn;)
+                    if (currentSpawnEnemy + 1 <= waves[currentWave].spawns[currentSpawn].qtd)
                     {
+                        
+                        if(timerSpawn > timeSpawn)
+                        {
+                            currentSpawnEnemy++;
+                            timerSpawn = 0;
+                            currentEnemy = Instantiate(waves[currentWave].spawns[currentSpawn].enemy, waves[currentWave].spawns[currentSpawn].rota[0].position, waves[currentWave].spawns[currentSpawn].rota[0].rotation);
+                            currentEnemy.GetComponent<Enemy>().SetRota(waves[currentWave].spawns[currentSpawn].rota);
 
-                        time = +1 * Time.deltaTime;
+                        }
+                        else
+                        {
+
+                            timerSpawn += 1 * Time.deltaTime;
+
+                        }
 
                     }
-                    */
+                    else
+                    {
+
+                        timer = 0;
+                        currentSpawnEnemy = 0;
+                        currentSpawn++;
+
+                    }
+
                 }
+                else
+                {
+
+                    timer += 1 * Time.deltaTime;
+
+                }
+            }
+            else
+            {
+
+                currentSpawn = 0;
+                currentWave++;
 
             }
+
         }
 
     }
+    
 
-}
-
-
-[Serializable]
-public class WaveData
-{
-    public SpawnData[] spawns;
-}
+    [Serializable]
+    public class WaveData
+    {
+        public SpawnData[] spawns;
+    }
 
 
-[Serializable]
-public class SpawnData
-{
+    [Serializable]
+    public class SpawnData
+    {
 
-    [Header("Tempo antes do spawn.")]
-    public float timeWaiting;
+        [Header("Tempo antes do spawn.")]
+        public float timeWaiting;
 
-    public GameObject enemy;
-    public int qtd;
-    public Transform[] rota;
+        public GameObject enemy;
+        public int qtd;
+        public Transform[] rota;
+
+    }
 
 }
