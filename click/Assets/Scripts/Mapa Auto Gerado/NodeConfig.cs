b135b026b7f35fd0ee0140d307b0391129be6da5
Daniel Mendes <80532809+danielregismendes@users.noreply.github.com>
2024-07-25 17:21:19 -0300

    0.025
    Ajustes nos tipos de Node.

--- click/Assets/Scripts/Mapa Auto Gerado/NodeConfig.cs
+++ click/Assets/Scripts/Mapa Auto Gerado/NodeConfig.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public enum NodeType
 {
-    Vila,
+    VilaVermelhaAzul,
+    VilaVermelhaAmarelo,
+    VilaAmareloAzul,
     Tesouro,
     Evento,
     Boss
@@ -15,5 +17,6 @@ public class NodeConfig : ScriptableObject
 {
     public Sprite sprite;
     public NodeType nodeType;
+
 
 }
